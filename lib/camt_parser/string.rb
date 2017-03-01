0b3700823888ad634d2fd4f73093dd8fe6dacedc
Damjan Cilenšek <vini@wicked.si>
2017-03-01 14:10:24 +0100

    Added XML parser

--- lib/camt_parser/string.rb
+++ lib/camt_parser/string.rb
@@ -2,18 +2,7 @@ module CamtParser
   class String
     def self.parse(raw_camt)
       doc = Nokogiri::XML raw_camt
-
-      namespaces = doc.namespaces['xmlns']
-      doc.remove_namespaces!
-
-      case namespaces
-      when 'urn:iso:std:iso:20022:tech:xsd:camt.052.001.02'
-        return CamtParser::Format052::Base.new(doc.xpath('Document'))
-      when 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.02'
-        return CamtParser::Format053::Base.new(doc.xpath('Document'))
-      else
-        raise CamtParser::Errors::UnsupportedNamespaceError, namespaces
-      end
+      CamtParser::Xml.parse(doc)
     end
   end
 end
