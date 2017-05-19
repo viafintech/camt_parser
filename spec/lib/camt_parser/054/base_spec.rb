90dced2120e572d53208bd76a94524447e9fb55f
Manuel Bouza <manuel@bouza.ch>
2017-05-19 17:14:49 +0200

    add support for camt.054

---
+++ spec/lib/camt_parser/054/base_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe CamtParser::Format054::Base do
+
+  context 'initialization' do
+    after do
+      CamtParser::File.parse 'spec/fixtures/054/valid_example.xml'
+    end
+
+    specify { expect(CamtParser::GroupHeader).to receive(:new).and_call_original }
+    specify { expect(CamtParser::Format054::Notification).to receive(:new).and_call_original }
+  end
+
+  let(:camt) { CamtParser::File.parse 'spec/fixtures/054/valid_example.xml' }
+  specify { expect(camt.group_header).to_not be_nil }
+  specify { expect(camt.notifications).to_not eq([]) }
+end
