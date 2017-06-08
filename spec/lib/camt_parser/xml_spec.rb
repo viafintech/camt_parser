2a9b242a5f03359d02010d03dd75fa5681d308d9
Khang Lam <khang.lam@sumcumo.com>
2017-06-08 20:21:17 +0200

    allow different subversions to be parsed

--- spec/lib/camt_parser/xml_spec.rb
+++ spec/lib/camt_parser/xml_spec.rb
@@ -9,15 +9,15 @@ describe CamtParser::Xml do
 
   context "parse" do
     it "raises an exception if it is not an XML" do
-      expect{ 
+      expect{
         described_class.parse 'not_xml'
       }.to raise_exception(CamtParser::Errors::NotXMLError, String)
     end
 
     it "raises an exception if the namespace/format is unknown" do
-      expect{ 
+      expect{
         described_class.parse parse_xml('spec/fixtures/general/invalid_namespace.xml')
-      }.to raise_exception(CamtParser::Errors::UnsupportedNamespaceError, 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.03')
+      }.to raise_exception(CamtParser::Errors::UnsupportedNamespaceError, 'urn:iso:std:iso:20022:tech:xsd:camt.053.002.03')
     end
 
     it "does not raise an exception for a valid namespace" do
