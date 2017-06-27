89a234e5cacef06b8715d44f9264c51c7cb2a594
Khang Lam <khang.lam@sumcumo.com>
2017-06-27 08:50:02 +0200

    declare explicit allowed versions

--- lib/camt_parser/xml.rb
+++ lib/camt_parser/xml.rb
@@ -7,11 +7,11 @@ module CamtParser
       doc.remove_namespaces!
 
       case namespaces
-      when /^urn:iso:std:iso:20022:tech:xsd:camt.052.001.0\d$/
+      when 'urn:iso:std:iso:20022:tech:xsd:camt.052.001.02'
         return CamtParser::Format052::Base.new(doc.xpath('Document'))
-      when /^urn:iso:std:iso:20022:tech:xsd:camt.053.001.0\d$/
+      when 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.02', 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.04'
         return CamtParser::Format053::Base.new(doc.xpath('Document'))
-      when /^urn:iso:std:iso:20022:tech:xsd:camt.054.001.0\d$/
+      when 'urn:iso:std:iso:20022:tech:xsd:camt.054.001.02', 'urn:iso:std:iso:20022:tech:xsd:camt.054.001.04'
         return CamtParser::Format054::Base.new(doc.xpath('Document'))
       else
         raise CamtParser::Errors::UnsupportedNamespaceError, namespaces
--- spec/lib/camt_parser/xml_spec.rb
+++ spec/lib/camt_parser/xml_spec.rb
@@ -17,7 +17,7 @@ describe CamtParser::Xml do
     it "raises an exception if the namespace/format is unknown" do
       expect{
         described_class.parse parse_xml('spec/fixtures/general/invalid_namespace.xml')
-      }.to raise_exception(CamtParser::Errors::UnsupportedNamespaceError, 'urn:iso:std:iso:20022:tech:xsd:camt.053.002.03')
+      }.to raise_exception(CamtParser::Errors::UnsupportedNamespaceError, 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.03')
     end
 
     it "does not raise an exception for a valid namespace" do
