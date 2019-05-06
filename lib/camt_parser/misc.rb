72526a029d5a0da8872aebadfc0ad17ec2a1a975
Tobias Schoknecht <tobias.schoknecht@barzahlen.de>
2019-05-06 12:18:13 +0200

    Return nil for missing amounts
    When no value is given for an amount field, we do not want to fail with
    an exception, as it is perfectly valid in camt files.
    Instead we want to return nil to make it clear that there is noving
    to convert into an integer or bigdecimal.
    That also prevents hiding that the value is missing, as would be the
    case in case of converting to 0/0.0
    We cannot make an assumption about whether one needs to differentiate it
    however, checking for nil is possible and default to 0.
    If we already default to 0, that becomes impossible.
    
    Refs #21

--- lib/camt_parser/misc.rb
+++ lib/camt_parser/misc.rb
@@ -2,11 +2,15 @@ module CamtParser
   class Misc
     class << self
       def to_amount_in_cents(value)
+        return nil if value == nil || value.strip == ''
+
         value.gsub(/[,|\.](\d*)/) { $1.ljust(2, '0') }.to_i
       end
 
       def to_amount(value)
-        BigDecimal value.gsub(',', '.')
+        return nil if value == nil || value.strip == ''
+
+        BigDecimal(value.gsub(',', '.'))
       end
     end
   end
