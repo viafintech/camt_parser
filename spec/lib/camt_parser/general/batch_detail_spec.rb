f0685fa9e2ddf6c3b08e3a6fa94e39df4ed34176
Tobias Schoknecht <tobias.schoknecht@barzahlen.de>
2020-03-06 15:30:30 +0100

    Make fixture and spec more realistic
    msg_id only allows 35 character values

--- spec/lib/camt_parser/general/batch_detail_spec.rb
+++ spec/lib/camt_parser/general/batch_detail_spec.rb
@@ -7,8 +7,8 @@ describe CamtParser::BatchDetail do
   let(:entries)        { ex_stmt.entries }
   let(:ex_entry)       { entries[0] }
   let(:batch_detail)   { ex_entry.batch_detail }
-  
+
   specify { expect(batch_detail.payment_information_identification).to eq("O0OpeAYTkhjerKu3eE9asw") }
   specify { expect(batch_detail.number_of_transactions).to eq("3") }
-  specify { expect(batch_detail.msg_id).to eq('02453b1e-7c11-4107-a777-ad9c273b4149') }
+  specify { expect(batch_detail.msg_id).to eq('02453b1e17c11241073a777ad9c273b4149') }
 end
