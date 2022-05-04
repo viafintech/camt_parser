f9afa8deb4792acce8760f4451f20ec0538aa863
Tobias Schoknecht <tobias.schoknecht@viafintech.com>
2022-05-04 18:26:29 +0200

    Add explanatory comment on variable names

--- lib/camt_parser/misc.rb
+++ lib/camt_parser/misc.rb
@@ -4,6 +4,7 @@ module CamtParser
       def to_amount_in_cents(value)
         return nil if value == nil || value.strip == ''
 
+        # Using dollars and cents as representation for parts before and after the decimal separator
         dollars, cents = value.split(/,|\./)
         cents ||= '0'
         format('%s%s', dollars, cents.ljust(2, '0')).to_i
