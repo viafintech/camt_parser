fc1091616fee7a207e39716e4c2320e8af6ac844
Olle Jonsson <olle.jonsson@gmail.com>
2023-04-28 07:56:14 +0200

    [docs] Add YARD docblocks to CamtParser::Account

--- lib/camt_parser/general/account.rb
+++ lib/camt_parser/general/account.rb
@@ -4,26 +4,32 @@ module CamtParser
       @xml_data = xml_data
     end
 
+    # @return [String]
     def iban
       @iban ||= @xml_data.xpath('Id/IBAN/text()').text
     end
 
+    # @return [String]
     def other_id
       @other_id ||= @xml_data.xpath('Id/Othr/Id/text()').text
     end
 
+    # @return [String]
     def account_number
       !iban.nil? && !iban.empty? ? iban : other_id
     end
 
+    # @return [String]
     def bic
       @bic ||= @xml_data.xpath('Svcr/FinInstnId/BIC/text()').text
     end
 
+    # @return [String]
     def bank_name
       @bank_name ||= @xml_data.xpath('Svcr/FinInstnId/Nm/text()').text
     end
 
+    # @return [String]
     def currency
       @currency ||= @xml_data.xpath('Ccy/text()').text
     end
