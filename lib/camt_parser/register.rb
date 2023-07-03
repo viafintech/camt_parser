891ee68147bff96679ee2b0cf2d1a4dbae6946ef
Manuel Bouza <manuel@bouza.ch>
2023-07-03 16:34:17 +0200

    Register namespace for version 8

--- lib/camt_parser/register.rb
+++ lib/camt_parser/register.rb
@@ -7,6 +7,7 @@ CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.052.001.02', :camt
 ## CAMT053
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.02', :camt053)
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.04', :camt053)
+CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.08', :camt053)
 
 ## CAMT054
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.054.001.02', :camt054)
