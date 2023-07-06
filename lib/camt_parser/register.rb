676bb3580e664de05bd4c8b96582457ca7c2687c
Daniel Puglisi <daniel@codegestalt.com>
2023-07-06 13:15:54 +0200

    add #booking_datetime and #value_datetime

--- lib/camt_parser/register.rb
+++ lib/camt_parser/register.rb
@@ -8,6 +8,7 @@ CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.052.001.02', :camt
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.02', :camt053)
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.04', :camt053)
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.08', :camt053)
+CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.053.001.10', :camt053)
 
 ## CAMT054
 CamtParser::Xml.register('urn:iso:std:iso:20022:tech:xsd:camt.054.001.02', :camt054)
